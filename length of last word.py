bb75ae10ab1421a8d69a00ef4f4ea97f9ec7be44
aditya23409876543 <129759837+aditya23409876543@users.noreply.github.com>
2023-10-21 16:23:23 +0530

    Add files via upload

---
+++ length of last word.py
@@ -0,0 +1,16 @@
+def length_of_last_word(s):
+    # Split the string by spaces
+    words = s.split()
+    
+    # Check if there are any words
+    if words:
+        # Return the length of the last word
+        return len(words[-1])
+    else:
+        # If there are no words, return 0
+        return 0
+
+# Example usage:
+s = input('sentence:')
+result = length_of_last_word(s)
+print(result) 
